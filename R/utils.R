b9bc9a033cd1287c9d9494a39d548471679567f1
Andrew Gene Brown <brown.andrewg@gmail.com>
2025-07-31 18:28:28 -0700

    simplify heuristic

--- R/utils.R
+++ R/utils.R
@@ -6,13 +6,27 @@
 #' a multiplicative factor for worker needs.
 #'
 #' @param x A _SpatRaster_ object.
-#' @param workers _integer_. Number of parallel workers. Default: `1`
-#' @param scl_need _numeric_. Scaling factor for memory needs. Default: `10`
-#' @param scl_workers _numeric_ .Scaling factor for each worker. Default: `1`
+#' @param workers _integer_. Number of parallel workers. Default uses value of
+#'   environment variable `R_RGEOMORPHON_N_WORKERS`. If unset, `1`
+#' @param scl_need _numeric_. Scaling factor for memory needs. Default uses
+#'   value of environment variable `R_RGEOMORPHON_MEM_SCALE_NEED`. If unset,
+#'   `10`.
+#' @param scl_workers _numeric_. Scaling factor for each worker. Default uses
+#'   value of environment variable `R_RGEOMORPHON_MEM_SCALE_WORKERS`. If unset,
+#'   `1`.
+#' @param pow_total _numeric_. Exponent for scaling total number of chunks.
+#'   Default uses value of environment variable `R_RGEOMORPHON_MEM_POWER`. If
+#'   unset, `1`.
 #'
 #' @returns _integer_. Number of tile chunks to divide `x` into.
 #' @noRd
-.terra_mem_chunks_needed <- function(x, workers = 1, scl_need = 10, scl_workers = 1) {
+.terra_mem_chunks_needed <- function(x,
+                                     workers = Sys.getenv("R_RGEOMORPHON_N_WORKERS", unset = 1),
+                                     scl_need = Sys.getenv("R_RGEOMORPHON_MEM_SCALE_NEED", unset = 10),
+                                     scl_workers = Sys.getenv("R_RGEOMORPHON_MEM_SCALE_WORKERS", unset = 1),
+                                     pow_total = Sys.getenv("R_RGEOMORPHON_MEM_POWER", unset = 1)) {
     mi <- as.data.frame(t(terra::mem_info(x, print = FALSE)))
-    ceiling(mi$needed * scl_need / (mi$available * (mi$memfrac / (workers * scl_workers)))) ^ 2
+    ceiling(mi$needed * as.numeric(scl_need) / (mi$available * (
+        mi$memfrac / (as.numeric(workers) * as.numeric(scl_workers))
+    )))^as.numeric(pow_total)
 }
